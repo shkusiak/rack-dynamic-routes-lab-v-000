72223e2f268391334409674823966823c234495c
Shea Kusiak <shkusiak@gmail.com>
2018-07-07 18:31:44 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find {|i| i.name == item_name}
+        resp.write item_name.price
+      else
+        resp.status = 400
+        resp.status "Item not found"
+      end
+    end
+    resp.finish
+  end
+
+end
